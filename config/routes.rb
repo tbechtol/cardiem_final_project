227231c5b103a9e49f1ea4841d019c72f7c955ff
Raghu Betina <raghu@firstdraft.com>
2016-11-22 17:29:26 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "cars#index"
   # Routes for the Service_notification resource:
   # CREATE
