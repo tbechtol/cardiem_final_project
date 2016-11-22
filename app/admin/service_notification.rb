c0d24315474b8ff1909f4ec6d65df12eb048dc02
Raghu Betina <raghu@firstdraft.com>
2016-11-22 17:29:45 +0000

    Whitelist columns in ServiceNotification for ActiveAdmin

--- app/admin/service_notification.rb
+++ app/admin/service_notification.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ServiceNotification do
 
+ permit_params :service_request_id, :status
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
