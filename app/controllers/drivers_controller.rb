3495943bf57d60f93b8fa7e84d081dc5a9f303ac
Tim Bechtold <tim.bechtold1@gmail.com>
2016-12-06 12:41:24 -0600

    enable_driver_update

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -17,7 +17,8 @@ class DriversController < ApplicationController
     @driver = Driver.new
 
     @driver.user_id = params[:user_id]
-
+    @driver.first_name = params[:first_name]
+    @driver.last_name = params[:last_name]
     save_status = @driver.save
 
     if save_status == true
@@ -31,6 +32,34 @@ class DriversController < ApplicationController
       end
     else
       render("drivers/new.html.erb")
+    end
+  end
+
+  def edit
+    @driver = Driver.find(params[:id])
+
+    render("drivers/edit.html.erb")
+  end
+
+  def update
+    @driver = Driver.find(params[:id])
+
+    @driver.user_id = params[:user_id]
+    @driver.first_name = params[:first_name]
+    @driver.last_name = params[:last_name]
+    save_status = @driver.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/drivers/#{@driver.id}/edit", "/update_driver"
+        redirect_to("/drivers/#{@driver.id}", :notice => "Driver updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Driver updated successfully.")
+      end
+    else
+      render("drivers/edit.html.erb")
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -61,14 +61,14 @@ Rails.application.routes.draw do
   # Created but commented out as this functionality will be handled via the Admin portal.
   # Users do not need to see others users or drivers.``
   devise_for :drivers
-  # # Routes for the Driver resource:
-  # # CREATE
-  # get "/drivers/new", :controller => "drivers", :action => "new"
-  # post "/create_driver", :controller => "drivers", :action => "create"
-  # #
-  # # # READ
-  # get "/drivers", :controller => "drivers", :action => "index"
-  # get "/drivers/:id", :controller => "drivers", :action => "show"
+  # Routes for the Driver resource:
+  # CREATE
+  get "/drivers/new", :controller => "drivers", :action => "new"
+  post "/create_driver", :controller => "drivers", :action => "create"
+  #
+  # READ
+  get "/drivers", :controller => "drivers", :action => "index"
+  get "/drivers/:id", :controller => "drivers", :action => "show"
   #
   #
   devise_for :users
